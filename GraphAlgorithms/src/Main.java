f6c7a9fa5adc70f348b7c8ad5d93e2c47aa67e69
Dalton <dalton.oyama@hotmail.com>
2022-08-29 23:17:43 -0300

    pratica 3

--- GraphAlgorithms/src/Main.java
+++ GraphAlgorithms/src/Main.java
@@ -1,33 +1,61 @@
-public class Main {
+import java.io.IOException;
+
+class Main {
+  public static void main(String[] args) throws IOException{
     
-    public static void main(String[] args){
-      
-      Graph g1 = new Graph(4); 
-    g1.addEdge(0, 1,3 );
-    g1.addEdge(1, 0,3 );
-    g1.addEdge(0, 3,4 );
-    g1.addEdge(3, 0,4 );
-    g1.addEdge(3, 4,2 );
-    System.out.println(g1);
+    System.out.println("\n");
+    Graph g1 = new Graph(10);
+    g1.addEdgeUnoriented(7, 5, 1);
+    g1.addEdgeUnoriented(7, 1, 1);
+    g1.addEdgeUnoriented(7, 2, 1);
+    g1.addEdgeUnoriented(1, 0, 1);
+    g1.addEdgeUnoriented(1, 4, 1);
+    g1.addEdgeUnoriented(2, 3, 1);
+    g1.addEdgeUnoriented(5, 6, 1);
+    g1.addEdgeUnoriented(6, 8, 1);
+    System.out.println("Busca largura");
+    System.out.println(g1.bfs(7));
+    System.out.println("-------------------------");
+    System.out.println("Busca profundidade");
+    System.out.println(g1.dfs(7));
+    
 
 
-  //numero de no   
-    System.out.println(g1.degree(0));
-    System.out.println(g1.degree(1));
-    System.out.println(g1.degree(2));
-    System.out.println(g1.degree(3));
+    System.out.println("\n");
+    System.out.println(g1.connected());
+    System.out.println("--------------------");
+    System.out.println(g1.dfsRec(7));
+    
 
+    
+    // Graph g1 = new Graph(4);
+    // g1.addEdge(0, 1, 1);
+    // g1.addEdge(1, 0, 1);
+    // g1.addEdge(0, 3, 1);
+    // g1.addEdge(3, 0, 1);
+    // System.out.println("=== g1 ===");
+    // System.out.println(g1);
+    // System.out.println("degree(0): " + g1.degree(0)); // 2
+    // System.out.println("degree(1): " + g1.degree(1)); // 1
+    // System.out.println("degree(2): " + g1.degree(2)); // 0
+    // System.out.println("degree(3): " + g1.degree(3)); // 1
+    // System.out.println("highestDegree: " + g1.highestDegree()); // 2
+    // System.out.println("lowestDegree: " + g1.lowestDegree()); // 0
+    // System.out.println("density: " + g1.density());
+    // System.out.println("\n=== g1 complement ===");
+    // System.out.println(g1.complement());
+    // Graph g2 = new Graph(3);
+    // g2.addEdge(0, 1, 1);
+    // g2.addEdge(1, 0, 1);
+    // System.out.println("g2 is subGraph? " + g1.subGraph(g2)); // true
+    // Graph g3 = new Graph(4);
+    // g3.addEdge(1, 3, 1);
+    // g3.addEdge(3, 1, 1);
+    // System.out.println("g3 is subGraph? " + g1.subGraph(g3)); // false
 
-    System.out.println("\nHighestDegree:");
-    System.out.println(g1.highestDegree());
+    System.out.println("\n");
+   Graph g2 = new Graph("C:\\Users\\DaltinhoOmito\\Desktop\\Java Vscode\\GraphAlgotithms\\GraphAlgorithms\\src\\Graph1.txt");
+    System.out.println(g2);
 
-    System.out.println("\nlowestDegree:");
-    System.out.println(g1.lowestDegree());
-     
-    System.out.println("\nComplement graph");
-    System.out.println(g1.comporplement());
-
-    }
-
-
+  }
 }
